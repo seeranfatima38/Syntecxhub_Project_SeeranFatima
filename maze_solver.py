94898f35af43effeeb192ef8f8e3f5e269fc10b3
seeranfatima38 <seeranfatima38@gmail.com>
2026-04-02 18:25:47 +0400

    Update maze_solver.py

--- maze_solver.py
+++ maze_solver.py
@@ -79,7 +79,20 @@ goal = (2, 2)
 
 path = astar(maze, start, goal)
 
+def print_maze_with_path(maze, path):
+    maze_copy = [row[:] for row in maze]
+
+    for (x, y) in path:
+        if maze_copy[x][y] not in ('S', 'G'):
+            maze_copy[x][y] = '*'
+
+    for row in maze_copy:
+        print(' '.join(row))
+
+
 if path:
     print("Path found:", path)
+    print("\nMaze with path:\n")
+    print_maze_with_path(maze, path)
 else:
     print("No path found")
